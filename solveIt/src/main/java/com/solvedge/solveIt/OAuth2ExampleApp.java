6f0ab7ee9736702a8670319fa52c75b9597507d5
priya <priyaforsolvedge@gmail.com>
2020-08-05 11:31:02 +0530

    Solveedge Demo

---
+++ solveIt/src/main/java/com/solvedge/solveIt/OAuth2ExampleApp.java
@@ -0,0 +1,41 @@
+package com.solvedge.solveIt;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+@SpringBootApplication
+@RestController
+public class OAuth2ExampleApp extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests(a -> a
+                        .antMatchers("/", "/error", "/css/**", "/js/**", "/images/**", "/assets/**").permitAll()
+                        .anyRequest().authenticated()
+                )
+                .oauth2Login();
+    }
+
+    @GetMapping("/api/user")
+    public Map<String, String> getUserInfo(@AuthenticationPrincipal OAuth2User user) {
+
+        Map<String, String> userInfo = new HashMap<>();
+        userInfo.put("email", user.getAttribute("email"));
+        userInfo.put("id",    user.getAttribute("sub"));
+
+        return userInfo;
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(OAuth2ExampleApp.class, args);
+    }
+}
